06fe73d4ff3890e4417182c175b35d7309090a51
Hack Reactor Student <student@hackreactor.com>
2017-07-10 16:31:20 -0700

    Refactor from functional to pseudoclassical
    Two tests passing

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,11 +1,11 @@
 describe('blinkyDancer', function() {
 
   var blinkyDancer, clock;
-  var timeBetweenSteps = 100;
+  var timeBetweenSteps = 100  ;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
